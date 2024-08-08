a1fcd189c3909589c12af9a9c8a5c807699bb1ee
Roman Mazurets <74185530+mazuretsroman@users.noreply.github.com>
2024-08-08 22:09:55 +0200

    add few tasks to server

--- server.cpp
+++ server.cpp
@@ -9,13 +9,69 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <vector>
+#include <regex>
+
+using namespace std;
 
 int checkStep(int res) {
   if (res == -1) {
-    std::cout << "Error: " << std::strerror(errno) << std::endl;
+    cout << "Error: " << strerror(errno) << endl;
     exit(EXIT_FAILURE);
   }
   return res;
+}
+
+namespace Task {
+  struct Base {
+    string helloMsg = "Sum: Give me array of less than 10 integers separated by space \' \'.";
+
+    string call(string args) { return args; };
+
+    protected:
+      vector<int> parsedArgs;
+
+      void parseArgs(string args) {
+	      regex regexp("[0-9]+");
+	      auto words_begin = sregex_iterator(args.begin(), args.end(), regexp);
+	      auto words_end = sregex_iterator();
+	      for (sregex_iterator i = words_begin; i != words_end; ++i)
+        {
+          smatch match = *i;
+          parsedArgs.push_back(stoi( match.str() ));
+        }
+      };
+  };
+
+  struct Sum : Base {
+    string call(std::string args) { 
+      parseArgs(args);
+
+      int sum = 0;
+      for ( int el : this->parsedArgs ) sum += el;
+
+      return to_string(sum);
+    }
+  };
+
+  struct Mult : Base {
+    string call(std::string args) { 
+      parseArgs(args);
+
+      int product = 1;
+      for ( int el : this->parsedArgs ) product *= el;
+
+      return to_string(product);
+    }
+  };
+
+  struct Max : Base {
+    string call(std::string args) { 
+      parseArgs(args);
+
+      return to_string( *max_element(this->parsedArgs.begin(), this->parsedArgs.end()) );
+    }
+  };
 }
 
 int main()
@@ -26,7 +82,11 @@ int main()
   addr.sin_family = AF_INET;
   addr.sin_port = htons(3001);
   inet_aton("127.0.0.1", &addr.sin_addr);
-  checkStep( bind(server, (struct sockaddr *)&addr, sizeof(addr)) );
+
+  // init awailable tasks
+  vector<Task::Base> availableTasks = { Task::Sum(), Task::Mult(), Task::Max() };
+
+  bind(server, (struct sockaddr *)&addr, sizeof(addr));
   checkStep( listen(server, 5) );
   std::cout << "Listening 3001..." << std::endl;
 
